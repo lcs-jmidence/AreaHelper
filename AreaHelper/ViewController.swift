c2a6a11ce5d7d1154285aee2e6acc2e709b78109
Julio Midence <jmidence@lcs.on.ca>
2019-10-16 20:11:19 -0400

    Initial Commit

---
+++ AreaHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AreaHelper
+//
+//  Created by Midence Rosario, Julio on 2019-10-16.
+//  Copyright © 2019 Julio Midence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
